e9cda37656aef5ed001e1999738622ad74f1c49b
Razeema <155413661+RazeemaRazik@users.noreply.github.com>
2026-01-21 14:05:32 +0530

    Add sign in and sign up fragments with initial main activity setup

---
+++ app/src/main/java/lk/razee/practical_1/adapter/ViewPagerAdapter.java
@@ -0,0 +1,30 @@
+package lk.razee.practical_1.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import lk.razee.practical_1.fragment.SignInFragment;
+import lk.razee.practical_1.fragment.SignUpFragment;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle){
+        super(fragmentManager,lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position){
+        if (position == 1){
+            return new SignInFragment();
+        }
+        return  new SignUpFragment();
+    }
+
+    @Override
+    public int getItemCount(){
+        return 2;
+    }
+}
